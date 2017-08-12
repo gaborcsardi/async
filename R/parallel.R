a4bda29c3044661573f74f7974ae89a1d2afd803
Gábor Csárdi <csardi.gabor@gmail.com>
2017-08-12 01:26:57 +0100

    add parallel()

---
+++ R/parallel.R
@@ -0,0 +1,15 @@
+
+#' @export
+
+parallel <- function(tasks, callback) {
+  l <- length(tasks)
+  result <- vector(mode = "list", length = l)
+  lapply(seq_along(tasks), function(i) {
+    tasks[[i]](function(err, res) {
+      if (!is.null(err)) callback(err)
+      l <<- l - 1
+      result[[i]] <<- res
+      if (l == 0) callback(NULL, result)
+    })
+  })
+}
---
+++ tests/testthat/test-parallel.R
@@ -0,0 +1,30 @@
+
+context("parallel")
+
+test_that("parallel", {
+
+  skip_if_offline()
+
+  res <- NULL
+
+  parallel(
+    list(
+      function(cb) {
+        http_get("https://eu.httpbin.org/get?q=foo",
+                 function(err, res) cb(err, rawToChar(res$content)))
+      },
+      function(cb) {
+        http_get("https://eu.httpbin.org/get?q=bar",
+                 function(err, res) cb(err, rawToChar(res$content)))
+      }
+    ),
+    function(err, result) { res <<- result }
+  )
+
+  await_all()
+  expect_equal(length(res), 2)
+  expect_false(is.null(res[[1]]))
+  expect_false(is.null(res[[2]]))
+  expect_match(res[[1]], "\"q\": \"foo\"", fixed = TRUE)
+  expect_match(res[[2]], "\"q\": \"bar\"", fixed = TRUE)
+})
