ac103fd24b44aa8b8f19bd45c7d544c50f0f68a0
Gábor Csárdi <csardi.gabor@gmail.com>
2017-08-12 20:59:01 +0100

    Extend whilst() test

--- tests/testthat/test-whilst.R
+++ tests/testthat/test-whilst.R
@@ -43,3 +43,26 @@ test_that("whilst with false test", {
 
   expect_null(result)
 })
+
+test_that("error", {
+
+  i <- 1
+  error <- result <- NULL
+  whilst(
+    function() i < 5,
+    function(callback) {
+      i <<- i + 1
+      if (i < 3) {
+        callback(NULL, "Good so far")
+      } else {
+        callback("This is bad", NULL)
+      }
+    },
+    function(err, res) { error <<- err; result <<- res }
+  )
+
+  await_all()
+
+  expect_null(result)
+  expect_equal(error, "This is bad")
+})
