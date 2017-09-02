86f6240018d5cc00a9726ad0fb8467d6fc7b5cac
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-02 10:22:50 +0100

    Refine await_any test case

--- tests/testthat/test-await-any.R
+++ tests/testthat/test-await-any.R
@@ -12,7 +12,9 @@ test_that("returns asap", {
 })
 
 test_that("fails asap", {
+  tic <- Sys.time()
   dx1 <- delay(1)$then(function(value) "foo")
   dx2 <- delay(1/10000)$then(function(value) stop("blah"))
   expect_error(await_any(dx1, dx2), "blah")
+  expect_true(Sys.time() - tic < as.difftime(1, unit = "secs"))
 })
