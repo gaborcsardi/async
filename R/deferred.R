24d782f368c51848be7afc5d8c702db232886110
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-02 09:38:30 +0100

    Add await_any()

--- R/deferred.R
+++ R/deferred.R
@@ -154,6 +154,18 @@ await_list <- function(..., .list = list()) {
   lapply(defs, get_value_x)
 }
 
+#' @export
+
+await_any <- function(..., .list = list()) {
+  defs <- c(list(...), .list)
+  states <- vcapply(defs, get_state_x)
+  while (all(states == "pending")) {
+    get_default_event_loop()$run("once")
+    states <- vcapply(defs, get_state_x)
+  }
+  get_value_x(defs[states != "pending"][[1]])
+}
+
 get_state_x <- function(x) {
   if (is.deferred(x)) x$get_state() else "not-deferred"
 }
