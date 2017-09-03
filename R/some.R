59438488f942dac711f51ff4bba3aa3c3a3dd6fa
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-03 11:05:26 +0100

    Readd some()

---
+++ R/some.R
@@ -0,0 +1,28 @@
+
+#' @export
+
+some <- function(.x, .p, ...) {
+  defs <- lapply(.x, async(.p), ...)
+  num_todo <- length(defs)
+  done <- FALSE
+
+  deferred$new(function(resolve, reject) {
+
+    if (length(defs) == 0) return(resolve(FALSE))
+
+    lapply(seq_along(defs), function(i) {
+      defs[[i]]$then(
+        function(value) {
+          if (!done && isTRUE(value)) {
+            done <<- TRUE
+            resolve(TRUE)
+          } else {
+            num_todo <<- num_todo - 1
+            if (num_todo == 0) resolve(FALSE)
+          }
+        },
+        function(reason) reject(reason)
+      )
+    })
+  })
+}
--- tests/testthat/test-some.R
+++ tests/testthat/test-some.R
@@ -3,39 +3,33 @@ context("some")
 
 test_that("some", {
 
-  skip("need to rewrite with deferred")  
-  
-  is_odd <- function(x, callback) callback(NULL, as.logical(x %% 2))
+  is_odd <- async(
+    function(x) delay(1/1000)$then(function(value) as.logical(x %% 2))
+  )
 
-  result <- NULL
-  wait_for(some(1:10, is_odd, function(err, res) result <<- res))
+  result <- await(some(1:10, is_odd))
   expect_identical(result, TRUE)
 
-  result <- NULL
-  wait_for(some(numeric(), is_odd, function(err, res) result <<- res))
+  result <- await(some(numeric(), is_odd))
   expect_identical(result, FALSE)
 
-  result <- NULL
-  wait_for(some(1:10 * 2, is_odd, function(err, res) result <<- res))
+  result <- await(some(1:10 * 2, is_odd))
   expect_identical(result, FALSE)
 
 })
 
 test_that("some, asyncify", {
 
-  skip("need to rewrite with deferred")  
-  
-  is_odd <- asyncify(function(x) as.logical(x %% 2))
+  is_odd <- async(
+    function(x) delay(1/1000)$then(function(value) as.logical(x %% 2))
+  )
 
-  result <- NULL
-  wait_for(some(1:10, is_odd, function(err, res) result <<- res))
+  result <- await(some(1:10, is_odd))
   expect_identical(result, TRUE)
 
-  result <- NULL
-  wait_for(some(numeric(), is_odd, function(err, res) result <<- res))
+  result <- await(some(numeric(), is_odd))
   expect_identical(result, FALSE)
 
-  result <- NULL
-  wait_for(some(1:10 * 2, is_odd, function(err, res) result <<- res))
+  result <- await(some(1:10 * 2, is_odd))
   expect_identical(result, FALSE)
 })
