781cbd09bdefdd8d7449793d7d57db7e03ebcf76
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-03 13:19:26 +0100

    Refine/simplify retry()

--- R/retry.R
+++ R/retry.R
@@ -2,18 +2,21 @@
 #' @export
 
 retry <- function(task, times) {
+  task <- async(task)
+  force(times)
+
   deferred$new(function(resolve, reject) {
 
     xresolve <- function(value) resolve(value)
     xreject  <- function(reason) {
       times <<- times - 1
       if (times > 0) {
-        async(task)()$then(xresolve, xreject)
+        task()$then(xresolve, xreject)
       } else {
         reject(reason)
       }
     }
 
-    async(task)()$then(xresolve, xreject)
+    task()$then(xresolve, xreject)
   })
 }
