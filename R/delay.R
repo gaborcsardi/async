211246758cf8c0ef41680896fb0a7750fc2d1831
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-03 20:39:08 +0100

    Remove callback arg of delay()

--- R/delay.R
+++ R/delay.R
@@ -7,15 +7,13 @@
 #' @param delay Time interval in seconds, the amount of time to delay
 #'   to delay the execution of the callback. It can be a fraction of a
 #'   second.
-#' @param callback The function to call after `delay` seconds. It will be
-#'   called without arguments.
 #' @return Task id, it can be waited on with [wait_for()].
 #'
 #' @export
 #' @examples
 #' TODO
 
-delay <- function(delay, callback = NULL) {
+delay <- function(delay) {
   deferred$new(function(resolve, reject) {
     force(resolve)
     force(reject)
