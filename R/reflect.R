7c1fdd6e50ff68b24b738e90af2e005dbbe06a00
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-04 12:08:56 +0100

    Fix reflect(), it must return a function

--- R/reflect.R
+++ R/reflect.R
@@ -3,8 +3,10 @@
 
 reflect <- function(task) {
   task <- async(task)
-  task()$then(
-    function(value)  list(error = NULL, result = value),
-    function(reason) list(error = reason, result = NULL)
-  )
+  function(...) {
+    task(...)$then(
+      function(value)  list(error = NULL, result = value),
+      function(reason) list(error = reason, result = NULL)
+    )
+  }
 }
