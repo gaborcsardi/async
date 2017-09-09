0d799f8136159f5462a6afdd31c921abb58e4537
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-09 01:49:00 +0100

    Add README, closes #9

--- R/package.R
+++ R/package.R
@@ -1,17 +1,3 @@
 
-#' Asynchronous HTTP
-#'
-#' @section Introduction:
-#' 
-#' The `async` package brings asynchronous computation and I/O to R.
-#'
-#' It has an event loop that takes care of scheduling multiple asynchronous
-#' functions, and polling for input from HTTP and other sources.
-#'
-#' TODO: more
-#'
-#' @docType package
-#' @name async
-#'
 #' @import rlang
 NULL
