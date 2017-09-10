a7dc8ed96f4ecafa5c38fde3b3c212173521ae38
Gábor Csárdi <csardi.gabor@gmail.com>
2017-09-10 10:18:54 +0100

    Fix timeout tests on windows
    We don't have a high resolution timer on windows.

--- tests/testthat/test-deferred-timeout.R
+++ tests/testthat/test-deferred-timeout.R
@@ -2,9 +2,11 @@
 context("deferred timeout")
 
 test_that("deferred timeout", {
+  skip_on_cran()
+
   good <- FALSE
   tic <- Sys.time()
   await(delay(1/4)$then(function(value) good <<- TRUE))
-  expect_true(Sys.time() - tic > as.difftime(1/4, unit = "secs"))
+  expect_true(Sys.time() - tic >= as.difftime(1/4, unit = "secs"))
   expect_true(good)
 })
