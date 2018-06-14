5ecb1bad97f33f51200391fbbe9aefac8ee03c81
Gábor Csárdi <csardi.gabor@gmail.com>
2018-06-14 09:45:33 -0500

    Fix a call test case, it is a race condition
    The crashing R process might report an exit or a closed
    connection.

--- tests/testthat/test-call-function.R
+++ tests/testthat/test-call-function.R
@@ -67,7 +67,11 @@ test_that("calls that crash", {
     )
   })
 
-  expect_error(synchronise(afun()), "R session crashed with exit code")
+  ## Either might happen here
+  err <- tryCatch(synchronise(afun()), error = function(x) x)
+  expect_true(
+    grepl("R session crashed with exit code", err$message) ||
+    grepl("R session closed the process connection", err$message))
 })
 
 test_that("handling call errors", {
