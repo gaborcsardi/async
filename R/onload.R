ca696e3f109231387f8aac2a7e489daee6fd1c23
Gábor Csárdi <csardi.gabor@gmail.com>
2018-11-16 11:34:11 +0000

    Don't need strrep and backports

--- R/onload.R
+++ R/onload.R
@@ -2,7 +2,6 @@
 ## nocov start
 
 .onLoad <- function(libname, pkgname) {
-  backports::import(pkgname, "strrep")
   ## This is the current max
   options(warning.length = 8170)
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
