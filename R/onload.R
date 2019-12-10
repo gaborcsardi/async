69a8587bdcbbaf9aba0c41317a2a130291be8950
Gábor Csárdi <csardi.gabor@gmail.com>
2019-12-10 13:57:28 +0000

    Improve vignette setup

--- R/onload.R
+++ R/onload.R
@@ -2,6 +2,11 @@
 ## nocov start
 
 .onLoad <- function(libname, pkgname) {
+  lazyrmd$onload_hook(
+    local = "if-newer",
+    ci = TRUE,
+    cran = FALSE
+  )
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
 }
 
