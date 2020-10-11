d6a679b596f18ee4629b9c3252df3ad85f10a95c
Gábor Csárdi <csardi.gabor@gmail.com>
2020-10-11 10:13:08 +0100

    Work around a partial match warning

--- R/utils.R
+++ R/utils.R
@@ -52,6 +52,7 @@ call_with_callback <- function(func, callback, info = NULL) {
         recerror <<- e
         recerror$aframe <<- recerror$aframe %||% find_async_data_frame()
         recerror$calls <<- recerror$calls %||% sys.calls()
+        if (is.null(recerror[["call"]])) recerror[["call"]] <<- sys.call()
         recerror$parents <<- recerror$parents %||% sys.parents()
         recerror[names(info)] <<- info
         handler <- getOption("async.error")
