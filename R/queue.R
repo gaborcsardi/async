ca337dba597a4a0cba0ac8233e41283330671a8c
Gábor Csárdi <csardi.gabor@gmail.com>
2020-11-05 13:08:33 +0000

    Async queue with task generator function

--- R/queue.R
+++ R/queue.R
@@ -1,45 +1,30 @@
 
-#' TODO:
+#' TODO
 #'
 #' @export
 
-async_queue <- function(run_limit = 1, input_limit = Inf) {
-
+async_queue <- function(task_generator, limit = 1) {
   assert_that(
-    input_limit == Inf || (is_count(input_limit) && input_limit >= 0),
-    run_limit == Inf || (is_count(run_limit) && run_limit >= 1)
+    is.function(task_generator),
+    limit == Inf || (is_count(limit) && limit >= 1)
   )
 
   done <- FALSE
-  queue_resolve <- NULL
-  waiting <- list()
-  ready <- list()
-  pushes <- list()
   pops <- list()
-  running <- 0L
+  ready <- list()
+  num_running <- 0L
 
-  # This is the promise for the queue itself. It will resolve when
-  # all tasks are done, and `$done()` has been called.
   queue <- deferred$new(
     type = "async_queue",
     call = sys.call(),
 
-    # When the queue is started, it is possible that it needs to resolve
-    # immediately, as laziness means that `$done()` might be called before
-    # the queue has started.
     action = function(resolve) {
-      queue_resolve <<- resolve
-      done <<- TRUE
+      maybe_queue()
+      maybe_resolve(resolve)
     },
 
-    # When a task finishes, we might need to
-    # * notify an async_pop() promise, otherwise store the result;
-    # * start another one;
-    # * resolve.
     parent_resolve = function(value, resolve, id) {
-      "!DEBUG Q task done `id`"
-      print("parent resolve")
-      running <<- running - 1L
+      num_running <<- num_running - 1L
       if (length(pops) > 0) {
         pop <- pops[[1]]
         pops <<- pops[-1]
@@ -48,86 +33,46 @@ async_queue <- function(run_limit = 1, input_limit = Inf) {
         ready[[length(ready) + 1]] <<- value
       }
       maybe_queue()
-      maybe_resolve()
+      maybe_resolve(resolve)
     }
   )
 
-  # We can queue a new task if we are under the limit and one is waiting.
-  # We can resolve a push() if not too many are waiting and one is pending.
+  is_exhausted <- function(x) identical(x, quote(exhausted))
+
   maybe_queue <- function() {
-    "!DEBUG Q task start attempt"
-    if (running < run_limit && length(waiting) > 0) {
-      "!DEBUG Q task start"
-      waiting[[1]]$then(queue)
-      waiting <<- waiting[-1]
-      running <<- running + 1L
-    }
-    if (length(waiting) < input_limit && length(pushes) > 0) {
-      "!DEBUG Q push allowed"
-      push <- pushes[[1]]
-      pushes <<- pushes[-1]
-      push(TRUE)
+    while (! done && num_running < limit) {
+      task <- task_generator()
+      if (is_exhausted(task)) {
+        done <<- TRUE
+      } else {
+        task$then(queue)
+        num_running <<- num_running + 1L
+      }
     }
   }
 
-  # We can resolve if `$done()` was called, no task is running, and
-  # there is nothing in the queue. If there still pops waiting then
-  # we'll resolve them to `quote(exhausted)`.
-  maybe_resolve <- function() {
-    "!DEBUG Q maybe resolve?"
-    print("done?")
-    if (done && length(ready) == 0 && running == 0 &&
-        !is.null(queue_resolve)) {
-      for (pop in pops) pop(quote(exhausted))
-      print("queue done")
-      queue_resolve(NULL)
+  maybe_resolve <- function(resolve) {
+    if (done && num_running == 0) {
+      for (pop in pops) pop(quote(exhauster))
+      resolve(NULL)
     }
   }
 
   list(
-    # nothing happens with the pushed promise, unless the promise returned
-    # by async_push() is waited on.
-    async_push = function(task) {
-      "!DEBUG Q push"
-      print("push")
-      pushes; waiting; maybe_queue
-      deferred$new(
-        type = "async_queue_push",
-        call = sys.call(),
-        # When the promise returned by `async_push()` is waited on
-        # we add the task to the waiting list, and maybe queue it.
-        action = function(resolve) {
-          print("push_action")
-          "!DEBUG Q push action"
-          pushes[[length(pushes) + 1]] <<- resolve
-          waiting[[length(waiting) + 1L]] <<- task
-          maybe_queue()
-          maybe_resolve()
-        }
-      )
-    },
-
-    # Returns a promise for _some_ result from the queue
     async_pop = function() {
-      print("pop")
-      ready; pops; maybe_resolve
       deferred$new(
         type = "async_queue_pop",
         call = sys.call(),
-        # If there is a result ready, we'll resolve with that.
-        # Otherwise we add the pop to the list that waits for results,
-        # and it will be called from the parent callback of the queue.
         action = function(resolve) {
-          print("pop action")
-          if (length(ready) > 0) {
+          if (done) {
+            resolve(quote(exhausted))
+          } else if (length(ready) > 0) {
             val <- ready[[1]]
             ready <<- ready[-1]
             resolve(val)
           } else {
-            pops[[length(pops) + 1]] <<- resolve
+            pops[[length(pops) + 1L]] <<- resolve
           }
-          maybe_queue()
-          maybe_resolve()
         }
       )
     },
--- tests/testthat/test-queue.R
+++ tests/testthat/test-queue.R
@@ -1,39 +1,71 @@
 
-test_that("async_queue", {
+secs <- function(x) as.difftime(x, units = "secs")
 
+test_that("async_queue", {
   do <- function() {
-    q <- async_queue(2, 1)
-    p1 <- q$async_push(async_constant(0)$then(function() 1))
-    p2 <- q$async_push(async_constant(0)$then(function() 2))
-    p3 <- q$async_pop()$then(function(x) print(paste("first:", x)))
-    p4 <- q$async_pop()$then(function(x) print(paste("second:", x)))
-    when_all(p1, p2, p3, p4, q$when_done())
+    taskgen <- local({
+      cnt <- 0L
+      function() {
+        cnt <<- cnt + 1L
+        if (cnt <= 2) {
+          mycnt <- cnt
+          delay(0.5)$then(function() mycnt)
+        } else {
+          quote(exhausted)
+        }
+      }
+    })
+
+    q <- async_queue(taskgen, limit = 1)
+    p1 <- q$async_pop()$then(function(x) paste("first:", x))
+    p2 <- q$async_pop()$then(function(x) paste("second:", x))
+    when_all(q$when_done(), p1, p2)
   }
 
-  synchronise(do())
-
+  expect_true(system.time(ret <- synchronise(do()))[3] >= secs(1))
+  expect_equal(ret[[2]], "first: 1")
+  expect_equal(ret[[3]], "second: 2")
 })
 
 test_that("async_queue", {
-
   do <- function() {
-    q <- async_queue(2, 1)
-    p1 <- q$async_push(async_constant(0)$then(function() 1))
-    p2 <- q$async_push(async_constant(0)$then(function() 2))
-    p3 <- q$async_pop()$then(function(x) print(paste("first:", x)))
-    p4 <- q$async_pop()$then(function(x) print(paste("second:", x)))
-    when_all(q$when_done(), p1, p2, p3, p4)
+    taskgen <- local({
+      cnt <- 0L
+      function() {
+        cnt <<- cnt + 1L
+        if (cnt <= 2) {
+          mycnt <- cnt
+          delay(0.5)$then(function() mycnt)
+        } else {
+          quote(exhausted)
+        }
+      }
+    })
+
+    q <- async_queue(taskgen, limit = 2)
+    p1 <- q$async_pop()$then(function(x) paste("first:", x))
+    p2 <- q$async_pop()$then(function(x) paste("second:", x))
+    when_all(p1, p2, q$when_done())
   }
 
-  synchronise(do())
-
+  expect_true(system.time(ret <- synchronise(do()))[3] < secs(1))
+  expect_equal(ret[[1]], "first: 1")
+  expect_equal(ret[[2]], "second: 2")
 })
 
 test_that("empty queue", {
-  skip("fails")
   do <- function() {
-    q <- async_queue(2, 1)
-    when_all(q$when_done())
+    taskgen <- local({
+      function() {
+        quote(exhausted)
+      }
+    })
+
+    q <- async_queue(taskgen, limit = 2)
+    p1 <- q$async_pop()$then(function(x) print(paste("first:", x)))
+    p2 <- q$async_pop()$then(function(x) print(paste("second:", x)))
+    when_all(q$when_done(), p1, p2)
   }
-  synchronise(do())
+
+  expect_output(system.time(synchronise(do())), "second: exhausted")
 })
