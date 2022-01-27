f900fc251702ae281b6d61c7f4a46ad592362437
Gábor Csárdi <csardi.gabor@gmail.com>
2022-01-28 00:34:14 +0100

    Relax timer test case

--- tests/testthat/test-timer.R
+++ tests/testthat/test-timer.R
@@ -64,27 +64,27 @@ test_that("mixing deferred and timers", {
       counter <<- counter + 1L
       if (counter == 3L) t$cancel()
     }
-    t <- async_timer$new(.1, cb)
+    t <- async_timer$new(.6, cb)
     delay(s)$then(function() "OK")
   }
 
   ## Once we have the output, we quit
   start <- Sys.time()
-  res <- synchronise(do(.15))
+  res <- synchronise(do(1))
   end <-  Sys.time()
 
   expect_equal(res, "OK")
-  expect_true(end - start >= as.difftime(.15, units = "secs"))
+  expect_true(end - start >= as.difftime(1, units = "secs"))
   expect_true(end - start <= as.difftime(2, units = "secs"))
   expect_equal(counter, 1L)
 
   ## Run the timer to the end
   start <- Sys.time()
-  res <- synchronise(do(.5))
+  res <- synchronise(do(3))
   end <-  Sys.time()
 
   expect_equal(res, "OK")
   expect_true(end - start >= as.difftime(1/5, units = "secs"))
-  expect_true(end - start <= as.difftime(2, units = "secs"))
+  expect_true(end - start <= as.difftime(5, units = "secs"))
   expect_equal(counter, 3L)
 })
