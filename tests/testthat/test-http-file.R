3ab202ba3b49151299ca4f1ece8d9f7fde5b66ee
Gábor Csárdi <csardi.gabor@gmail.com>
2022-07-15 14:56:54 +0200

    Test cases for http:// URLs

---
+++ tests/testthat/test-http-file.R
@@ -0,0 +1,47 @@
+
+context("http file:://")
+
+test_that("GET file://", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  cat("foobar\n", file = tmp)
+
+  url <- paste0("file://", normalizePath(tmp))
+  ret <- synchronise(http_get(url)$then(http_stop_for_status))
+  expect_equal(ret$status_code, 0)
+  expect_equal(
+    ret$content,
+    as.raw(c(0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72, 0x0a))
+  )
+})
+
+test_that("HEAD file://", {
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  cat("foobar\n", file = tmp)
+
+  url <- paste0("file://", normalizePath(tmp))
+  ret <- synchronise(http_head(url)$then(http_stop_for_status))
+  expect_equal(ret$status_code, 0)
+})
+
+test_that("file:// to file", {
+  tmp <- tempfile()
+  tmp2 <- tempfile()
+  on.exit(unlink(c(tmp, tmp2)), add = TRUE)
+  cat("foobar\n", file = tmp)
+
+  url <- paste0("file://", normalizePath(tmp))
+  ret <- synchronise(http_get(url, file = tmp2)$then(http_stop_for_status))
+  expect_equal(ret$status_code, 0)
+  expect_equal(
+    readBin(tmp2, "raw", 100),
+    as.raw(c(0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72, 0x0a))
+  )
+})
+
+test_that("file:// does not exist", {
+  tmp <- tempfile()
+  url <- paste0("file://", tmp)
+  expect_error(synchronise(http_get(url)))
+})
