d8ab57d76a5450f6a862e8de38517bd2523b1f8b
Gábor Csárdi <csardi.gabor@gmail.com>
2022-07-23 09:51:51 +0200

    Remove lazyrmd

--- R/onload.R
+++ R/onload.R
@@ -2,11 +2,6 @@
 ## nocov start
 
 .onLoad <- function(libname, pkgname) {
-  lazyrmd$onload_hook(
-    local = "if-newer",
-    ci = TRUE,
-    cran = FALSE
-  )
   if (requireNamespace("debugme", quietly = TRUE)) debugme::debugme()
 }
 
